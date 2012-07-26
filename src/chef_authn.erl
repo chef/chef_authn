8f57f5913c0ecdcaad7ae0c7c38d6c4a9e3b091d
doubt72 <doug@opscode.com>
2012-07-26 16:32:25 -0600

    Added boilerplate to source files + Apache license

--- src/chef_authn.erl
+++ src/chef_authn.erl
@@ -2,12 +2,29 @@
 %% ex: ts=4 sw=4 et
 %% @author Seth Falcon <seth@opscode.com>
 %% @author Christopher Brown <cb@opscode.com>
-%% @copyright 2011 Opscode, Inc.
 %% @doc chef_authn - Request signing and authentication for Opscode Chef
 %%
 %% This module is an Erlang port of the mixlib-authentication Ruby gem.
 %% It can be used to sign HTTP requests to send to a Chef server or to
 %% validate such requests (for server implementation).
+%%
+%% Copyright 2011-2012 Opscode, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+
 
 -module(chef_authn).
 -include_lib("eunit/include/eunit.hrl").
